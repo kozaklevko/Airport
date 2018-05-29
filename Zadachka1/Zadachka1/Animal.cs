def88b8951649795ec947d87668dfa9560308e83
kozaklevko <kozaklevko@rambler.ru>
2018-05-29 21:34:35 +0300

    1
    1

---
+++ Zadachka1/Zadachka1/Animal.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadachka1
+{
+    abstract class Animal
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int Age { get; protected set; }
+        public abstract int AgeCoef { get; }
+
+        public Animal (string name)
+        {
+            this.Name = name;
+        }
+
+        public abstract void Voice();
+        public abstract void AddAge(int age);
+
+    }
+    class Dog : Animal
+    {
+        public Dog(string name):base(name)
+        {
+
+        }
+        public override int AgeCoef { get; } = 10;
+
+        public override void AddAge(int age)
+        {
+            this.Age = age * this.AgeCoef;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public override void Voice()
+        {
+            Console.WriteLine("Gav-Gav"); 
+        }
+    }
+    class Cat : Animal
+    {
+        public Cat(string name):base(name)
+        {
+
+        }
+        public override int AgeCoef { get; } = 7;
+
+        public override void AddAge(int age)
+        {
+            this.Age = age * this.AgeCoef;
+        }
+
+        public override void Voice()
+        {
+            Console.WriteLine("Meow-Meow"); 
+        }
+    }
+
+    class Bird : Animal
+    {
+        public override int AgeCoef { get; }=100;
+
+        public override void AddAge(int age)
+        {
+            this.Age = age / this.AgeCoef;
+        }
+
+        public override void Voice()
+        {
+            Console.WriteLine("Car-car"); 
+        }
+    }
+    class Parrot : Bird
+    {
+        public Parrot(string name):base(name)
+        {
+        }
+        public override void Voice()
+        {
+            Console.WriteLine("Parrot"); ;
+        }
+    }
+    class Cenery : Bird
+    {
+        public Cenery(string name):base(name)
+        {
+        }
+        public override void Voice()
+        {
+            Console.WriteLine("Canery"); 
+        }
+       // public new void AddAge();
+
+    }
+
+
+}
---
+++ Zadachka1/Zadachka1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadachka1
+{
+    class Program
+    {
+        static Animal[] animal_array = new Animal[5];
+        static void Main(string[] args)
+        {
+           
+            animal_array[0] = new Dog("Sharik");
+            animal_array[1] = new Dog("Brovko");
+            animal_array[2] = new Cat("Murchik");
+            animal_array[3] = new Parrot("Kesha");
+            animal_array[4] = new Cenery("Cenery");
+            //Animal[] birds = GetAnimal(typeof(Bird));
+            foreach(Animal el in animal_array)
+            {
+                el.Voice();
+                el.AddAge(IReadOnlyCollection.)
+            }
+
+        }
+        public static Animal GetAnimal(Type animalType)
+        {
+            int count = 0;
+            foreach (Animal element in animal_array)
+            {
+                if(element.GetType() == animalType)
+                {
+                    count++;
+                }
+            }
+            foreach (Animal element in animal_array)
+            {
+                if (element.GetType() == animalType)
+                {
+                    count++;
+                }
+            }
+            Animal[] animal_arr = new Animal[count];
+            for (int i=0;i<count;i++)
+            {
+                Animal element = animal_array[i];
+                if(element.GetType() == animalType)
+                {
+                    animal_arr = animal_array;
+                }
+            }
+        }
+
+    }
+}
